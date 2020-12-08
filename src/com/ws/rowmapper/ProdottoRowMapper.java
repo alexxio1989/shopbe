7bd0eae2d3d29bda4b2e4fbe5e39d1ed71ddfb04
Alessio Pinna <alessiopinna@MacBook-Air-di-Alessio.local>
2020-12-08 09:55:57 +0100

    sviluppi

--- src/com/ws/rowmapper/ProdottoRowMapper.java
+++ src/com/ws/rowmapper/ProdottoRowMapper.java
@@ -3,18 +3,29 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 import com.ws.models.Prodotto;
 import com.ws.models.SubDominio;
 import com.ws.repository_impl.SottoTipoRepoImpl;
+import com.ws.utils.JdbcUtil;
 
 @Component
 public class ProdottoRowMapper implements RowMapper<Prodotto> {
 
+ 
+    
     @Autowired
-    private SottoTipoRepoImpl repoSubType;
+    private JdbcUtil jdbcUtil;
+    
+    @Autowired
+    private SottoTipoRowMapperLite trrm;
+    
+    @Value("${sotto.tipo.get}")
+    protected String queryGet;
+
 
     @Override
     public Prodotto mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -24,7 +35,7 @@ public class ProdottoRowMapper implements RowMapper<Prodotto> {
         prodotto.setDescrizione(rs.getString("descrizione"));
         prodotto.setImage(rs.getString("image"));
         prodotto.setPrezzo(rs.getBigDecimal("prezzo"));
-        prodotto.setTipo(repoSubType.get(new SubDominio(rs.getInt("sotto_tipo_idsotto_tipo"))));
+        prodotto.setTipo(jdbcUtil.queryForObj(queryGet, new Object[] {rs.getInt("sotto_tipo_idsotto_tipo")} , trrm));
         return prodotto;
     }
     
---
+++ src/com/ws/rowmapper/SottoTipoRowMapperLite.java
@@ -0,0 +1,33 @@
+package com.ws.rowmapper;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import com.ws.models.Prodotto;
+import com.ws.models.SubDominio;
+import com.ws.utils.JdbcUtil;
+
+@Component
+public class SottoTipoRowMapperLite implements RowMapper<SubDominio> {
+	
+	
+	@Value("${get.prodotti.associati}")
+    protected String queryGetProdottiAssociati;
+
+    @Override
+    public SubDominio mapRow(ResultSet rs, int rowNum) throws SQLException {
+        SubDominio dominio = new SubDominio();
+        dominio.setId(rs.getInt("idsotto_tipo"));
+        dominio.setCodice(rs.getString("codice"));
+        dominio.setDescrizione(rs.getString("descrizione"));
+        return dominio;
+    }
+    
+}
